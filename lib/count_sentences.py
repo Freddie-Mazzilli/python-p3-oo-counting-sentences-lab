e410d848a0cea366ab0a75fc94d4eb813a5ef79f
Freddie Mazzilli <freddie.mazzilli@gmail.com>
2023-05-31 15:06:55 -0400

    Completes oo counting sentences lab

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,42 @@
+
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+
+  def __init__(self, value=""):
+    self.value = value
+
+  def is_sentence(self):
+    return self.value.endswith(".")
+  
+  def is_question(self):
+    return self.value.endswith("?")
+
+  def is_exclamation(self):
+    return self.value.endswith("!")
+
+  def count_sentences(self):
+    count = 0
+    prev = ""
+    punct = [".", "!", "?"]
+    for i in self.value:
+      if i in punct and not i == prev:
+        count += 1
+      prev = i
+    return count
+      
+    # sentences = re.split('[.!?]', self.value)
+    # non_empty_sentences = [w for w in sentences if not w == ""]
+    # return len(non_empty_sentences)
+
+  def get_value(self):
+    return self._value
+
+  def set_value(self, value):
+    if type(value) == str:
+      self._value = value
+    else:
+      print("The value must be a string.")
+
+  value = property(get_value, set_value)
